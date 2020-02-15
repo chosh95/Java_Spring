1a38e1b50f6a959819b5905d15360223ef2ba752
chosh95 <chosh-95@hanmail.net>
2020-02-15 23:01:22 +0900

    0215

--- eatgo/eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
+++ eatgo/eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -10,14 +10,15 @@ class RestaurantTests {
 
     @Test
     public void creation(){
-        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        assertThat(restaurant.getId(), is(1004L));
         assertThat(restaurant.getName(), is("Bob zip"));
         assertThat(restaurant.getAddress(),is("Seoul"));
     }
 
     @Test
     public void information(){
-        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
 
         assertThat(restaurant.getInformation(),is("Bob zip in Seoul"));
     }
---
+++ eatgo/eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -0,0 +1,39 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RestaurantController.class)
+public class RestaurantControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void list() throws Exception {
+        mvc.perform(get("/restaurants"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("\"id\":1004")))
+            .andExpect(content().string(containsString("\"name\":\"Bob zip\"")));
+    }
+
+    @Test
+    public void detail() throws Exception {
+        mvc.perform(get("/restaurants/1004"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"id\":1004")))
+                .andExpect(content().string(containsString("\"name\":\"Bob zip\"")));
+
+        mvc.perform(get("/restaurants/2020"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"id\":2020")))
+                .andExpect(content().string(containsString("\"name\":\"Cyber Food\"")));
+    }
+}
