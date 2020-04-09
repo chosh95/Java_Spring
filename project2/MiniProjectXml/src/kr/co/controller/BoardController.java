95f8c07d2a14f9acfd4209b043e6b34f2d77db36
chosh95 <chosh-95@hanmail.net>
2020-04-09 20:35:12 +0900

    ch_42

---
+++ project2/MiniProjectXml/src/kr/co/controller/BoardController.java
@@ -0,0 +1,15 @@
+package kr.co.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@GetMapping("/main")
+	public String main() {
+		return "board/main";
+	}
+}
