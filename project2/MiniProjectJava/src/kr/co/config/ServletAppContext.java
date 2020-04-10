6f2c5aca71868dbb2baa0da2c40a0d9980051357
chosh95 <chosh-95@hanmail.net>
2020-04-10 19:45:25 +0900

    db_setting

--- project2/MiniProjectJava/src/kr/co/config/ServletAppContext.java
+++ project2/MiniProjectJava/src/kr/co/config/ServletAppContext.java
@@ -1,11 +1,20 @@
 package kr.co.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import kr.co.mapper.BoardMapper;
 
 //Spring MVC 프로젝트에 관련된 설정을 하는 클래스
 @Configuration
@@ -13,7 +22,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 //스캔할 패키지를 지정한다.
 @ComponentScan("kr.co.controller")
+@PropertySource("/WEB-INF/properties/db.properties")
 public class ServletAppContext implements WebMvcConfigurer{
+	
+	@Value("${db.classname}")
+	private String db_classname;
+	
+	@Value("${db.url}")
+	private String db_url;
+
+	@Value("${db.username}")
+	private String db_username;
+	
+	@Value("${db.password}")
+	private String db_password;
+	
 	//controller의 메소드가 반환하는 jsp의 이름 앞뒤에 경로와 확장자를 붙여준다.
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
@@ -27,5 +50,35 @@ public class ServletAppContext implements WebMvcConfigurer{
 		// TODO Auto-generated method stub
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+	
+	//db 접속정보 관리
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource source = new BasicDataSource();
+		source.setDriverClassName(db_classname);
+		source.setUrl(db_url);
+		source.setUsername(db_username);
+		source.setPassword(db_password);
+		
+		return source;
+	}
+	
+	//쿼리문, 접속정보 관리
+	@Bean
+	public SqlSessionFactory factory(BasicDataSource source) throws Exception{
+		
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(source);
+		SqlSessionFactory factory = factoryBean.getObject();
+		return factory;
+	}
+	
+	//쿼리문 실행을 위한 객체 (Mapper 관리)
+	@Bean
+	public MapperFactoryBean<BoardMapper> getBoardMapper(SqlSessionFactory factory) throws Exception{
+		MapperFactoryBean<BoardMapper> factoryBean = new MapperFactoryBean<BoardMapper>(BoardMapper.class);
+		factoryBean.setSqlSessionFactory(factory);
+		return factoryBean;
 	}
 }
---
+++ project2/MiniProjectJava/src/kr/co/mapper/BoardMapper.java
@@ -0,0 +1,5 @@
+package kr.co.mapper;
+
+public interface BoardMapper {
+
+}
